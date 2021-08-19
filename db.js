c46b734dd4ec923bee2350705f3f34dab3f5b304
srinivas79y <yelurusrinivas79@gmail.com>
2021-08-19 16:00:36 +0530

    Initial commit

---
+++ db.js
@@ -0,0 +1,33 @@
+// db.js
+
+// mongodb driver
+const MongoClient = require("mongodb").MongoClient;
+
+const dbConnectionUrl = "mongodb+srv://srinivas:srinivas123@cluster0.2dvnh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+function initialize(
+    dbName,
+    dbCollectionName,
+    successCallback,
+    failureCallback
+) {
+    MongoClient.connect(dbConnectionUrl, function(err, dbInstance) {
+        if (err) {
+            console.log(`[MongoDB connection] ERROR: ${err}`);
+            failureCallback(err); // this should be "caught" by the calling function
+        } else {
+            const dbObject = dbInstance.db(dbName);
+            const dbCollection = dbObject.collection(dbCollectionName);
+            console.log("[MongoDB connection] SUCCESS");
+
+            successCallback(dbCollection);
+        }
+    });
+}
+
+
+
+
+module.exports = {
+    initialize
+};
---
+++ server.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const server = express();
+
+const body_parser = require("body-parser");
+
+// parse JSON (application/json content-type)
+server.use(body_parser.json());
+
+const port = 4000;
+
+// << db setup >>
+const db = require("./db.js");
+const dbName = "Nutrition";
+const collectionName = "ProductDesc";
+
+db.initialize(dbName, collectionName, function(dbCollection) { // successCallback
+    // get all items
+    dbCollection.find().toArray(function(err, result) {
+        if (err) throw err;
+          console.log(result);
+    });
+
+    server.get("/productInfo", (request, response) => {
+        const productName = request.query.name;
+        
+        console.log(productName)
+        console.log(dbCollection)
+
+        dbCollection.find().toArray((error, result) => {
+            if (error) throw error;
+            // return item
+            response.json(result);
+        });
+    });
+
+    server.get("/productData", (request, response) => {
+       
+      
+
+
+        dbCollection.findOne({name:"Sorghum"}, (error, result) => {
+            if (error) throw error;
+            // return item
+
+            console.log(result)
+            response.json(result);
+        });
+           
+        });
+
+
+    server.get("/productDatas", (request, response) => {
+       
+        
+
+        var productName= request.query.productName;
+        console.log(productName);
+
+        var query ={
+            name: productName
+        }
+
+        console.log(query)
+        dbCollection.findOne(query, (error, result) => {
+            if (error) throw error;
+            // return item
+
+            console.log(result)
+            response.json(result);
+        });
+           
+       
+    });
+
+});
+server.listen(port, () => {
+    console.log(`Server listening at ${port}`);
+});
+
+
+
